f15dbf47a835b3a7b2cdc7b2e83bf002de6d29c0
hamza <hamzashakil181@gmail.com>
2024-09-06 16:42:49 -0700

    work complete

---
+++ app.js
@@ -0,0 +1,197 @@
+// ASSIGNMENT NO 1
+
+// // Chapter 1 (Alerts) 
+// // ques1)
+// // 1)
+// alert("Firstname: MUHAMMAD");
+// // 2)
+// alert("last name:HAMZA")
+// // 3)
+// alert("Email:hamzashakil181@gmail.com")
+// // 4)
+// alert("phone-number:03243287086")
+// // 5)
+// alert("password:11111")
+
+// // Ques:2)
+// alert("You're learning JavaScript!")
+
+// // Ques:3)
+// alert("Hello,Javascript!")
+
+
+
+// // Chapter 2(Variables for string)
+// // Ques:1)
+// // camel case:
+// var firstName = "muhammad";
+
+// // Ques:2)
+// var myName;
+// myName = "hamza";
+// console.log(myName);
+
+
+// // Ques:3)
+// var teamName;
+// teamName = "hamza's-team";
+// alert(teamName);
+
+// // Ques:4)
+// var bestMan = "Charlie";
+// bestMan = "john";
+// alert(bestMan);
+
+
+
+// // Chapter 3 (Variables for numbers) 
+// // Ques:1)
+// var caseQty;
+
+// // Ques:2)
+// caseQty = 144;
+
+// // Ques:3)
+// var num =9;
+
+// // Ques:4)
+// var total;   
+// total = 5 + 3;  
+
+// // Ques:5)
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+
+// orderTotal = 100 + 10;  
+// orderTotal = 110
+
+// // Ques:6)
+// var myNumber = 20;    
+// myNumber = myNumber + 15; 
+
+
+// PRACTICE QUESTION
+
+// // CHAP 1 (ALERT)
+
+// // Q1:
+// alert("hello world!")
+
+// // Q2:
+// alert("Error! Please enter a valid password.") 
+
+// // Q3:
+// alert("Welcome to JS land..."
+//     + "Happy Coding!"
+// )
+
+// // Q4:
+// alert("Welcome to JS land")
+// alert("Happy Coding!")
+
+// // Q5:
+// console.log("Hello..I can run JS through my web browser console ")
+
+// // Q6:
+// A : YES
+// B : YES
+// C : YES
+// D : YES
+
+
+// // CHAP#2(VARIABLE FOR STRING)
+
+// // Q1:
+// var username;
+// // Q2:
+// var myName = "Muhammad Hamza"
+// // Q3:
+// // a:
+// var message;
+// // b:
+//  message = "Hello World";
+// // c
+// alert(message)
+
+// // Q4:
+// alert("muhammad hamza")
+// alert("21 years old")
+// alert("certified mobile application development")
+
+// // Q5
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+// // Q6
+// var email ="hamzashakil181@gmail.com"
+// alert("my email address is "+ email )
+
+// // Q7
+// var book ="A smarter way to learn JavaScript"
+// alert("I am trying to learn from the book " + book )
+
+// // Q8
+// // document.write("yah! i can write HTML content through javascript")
+
+// // Q9
+// var end ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(end)
+
+
+// // CHAP # 03(VARIABLE FOR NUMBERS)\
+
+// //Q1
+// var age;
+// age ="i am 21 years old"
+// alert(age)
+
+// // Q2
+// var num ="12"
+// alert("you have visited this site " + num + " times"  )
+
+// // Q3
+// var birthYear = 2004 
+// document.write("my birth year is " + birthYear)
+// document.write(typeof "birthYear")
+
+// // Q4
+// var visitorName = "john doe"
+// var productTitle ="XYZ clothing store"
+// var quantity ="ordered 5 T-shirt(s)"
+
+// document.write(visitorName + quantity + productTitle)
+
+
+// chap 04 {legal and illegal}
+
+// // Q1
+// var myName = "Muhammad Hamza",
+//  city ="karachi",
+//   education = "bachelors";
+
+// // Q2:
+// LEGAL:
+// var myVariable;
+// var $dollar;
+// var _underscore;
+// var camelCase;
+// var variable123;
+
+// ILLEGAL:  
+// var 1variable;    
+// var var#name;   
+// var var-name;    
+// var var name;     
+// var let;   
+
+// // Q3
+// // a
+// document.write("<h1>Rules for naming JS variables</h1>");
+// // b
+// document.write("<p>Variable names can only contain ,numbers,$ and _. For example $my_1stVariable </p>")
+// // c
+// document.write("<p> Variables must begin with a letter, _ or $. For example $name, _name or name </p>")
+// // d
+// document.write("<p> Variable names are case sensitive</p>")
+// // e
+// document.write("<p>Variable names should not be JS reserved keywords</p>")
